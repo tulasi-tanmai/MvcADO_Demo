36f3d3deb8224a607d963a3c7f3dfd42ba450429
tulasi-tanmai <uraghtanmai@gmail.com>
2025-08-25 18:28:46 +0530

    add delete

--- obj/Debug/net9.0/MvcADO_Demo.AssemblyInfo.cs
+++ obj/Debug/net9.0/MvcADO_Demo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcADO_Demo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96ccb827f7fdee47cb140fd08757a06d6afa76c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcADO_Demo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcADO_Demo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
